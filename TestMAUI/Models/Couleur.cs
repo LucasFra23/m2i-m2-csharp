7dd9147b3eaa3cbd72879c7f42c21cf69ecf28a8
Lucas <lucas.francois2301@gmail.com>
2025-03-10 16:02:32 +0100

    Add collection view colors

---
+++ TestMAUI/Models/Couleur.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMAUI.Models
+{
+    public class Couleur
+    {
+        public string Name { get; set; }
+        public string BoxViewColor { get; set; }
+    }
+}
