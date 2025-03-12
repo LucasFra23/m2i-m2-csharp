fd92d175457b9373085950263ca15d339afbb955
Lucas <lucas.francois2301@gmail.com>
2025-03-12 09:29:35 +0100

    Init Puissance 4 MAUI

---
+++ Puissance4MAUI/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Puissance4MAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ Puissance4MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Puissance4MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
